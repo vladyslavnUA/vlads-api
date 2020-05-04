c22330fe64bf1a801ff404cd494151bef4ab1b9b
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-04 13:35:32 -0700

    updated server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const util = require('util');
+
+require('dotenv').config();
+
+const app = require('./config/express');
+const router = require('./controllers/index.js');
+
+mongoose.Promise = Promise;
+
+const mongoUri = process.env.MONGODB_URI;
+mongoose.connect(
+  mongoUri,
+  { server: { socketOptions: { keepAlive: 1 } } }
+);
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+app.use(router);
+
+if (!module.parent) {
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log(`app listening on port ${port}`);
+    });
+}
+
+module.exports = app;
