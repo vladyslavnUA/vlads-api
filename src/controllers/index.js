859465811654f4d788d65a0f66638f3120d5112b
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 15:21:07 -0400

    updated main index  controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bookRoutes = require('./books.js');
 const authRoutes = require('./auth.js');
-
 const router = express.Router();
 
-// router.use('', bookRoutes);
+router.use('', bookRoutes);
 router.use('/auth', authRoutes);
 
 // router.get('/', (req, res) => {
