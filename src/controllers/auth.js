71030a97145c67cf21f5c93713af8751d27a71a2
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 15:19:27 -0400

    updated authentication controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,36 +1,34 @@
 const express = require('express');
-const User = require('../models/user');
+const User = require('../models/user.js');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 // SIGN UP FORM
-router.get("/sign-up", (req, res) => {
-    res.render("sign-up");
-});
+// router.get("/sign-up", (req, res) => {
+//     res.render("sign-up");
+// });
 
-// SIGN UP POST
+// SIGN UP
 router.post("/sign-up", (req, res) => {
     // Create User and JsonWebToken
     const user = new User(req.body);
 
-    user
-        .save()
-        .then(user => {
-        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+    user.save().then(user => {
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
         res.cookie('nToken', token, { maxAge: 900000 , httpOnly: true });
+        res.json({'jwt-token': token})
         console.log("new sign-up token: " + token)
-        res.redirect("/");
-        })
-        .catch(err => {
+        // res.redirect("/");
+        }).catch(err => {
         console.log(err.message);
         return res.status(400).send({ err: err });
         });
-});
+})
 
 // LOGIN FORM
-router.get('/login', (req, res) => {
-    res.render('login');
-});
+// router.get('/login', (req, res) => {
+//     res.render('login');
+// });
 
 // LOGIN
 router.post("/login", (req, res) => {
@@ -52,11 +50,12 @@ router.post("/login", (req, res) => {
             // create a token
             const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
                 expiresIn: "60 days"
-            })
+            });
             // set a cookie and redirect to root
             res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-            res.redirect("/");
-            })              
+            res.json({'login:': 'great success', 'user-signed-in:': user})
+            // res.redirect("/");
+            });              
         }).catch(err => {
             console.log(err);
         });
