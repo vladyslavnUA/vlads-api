458138b70adee74d9b7d7ef5d40f4cbd02d95308
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 12:42:33 -0400

    updated user model

--- src/models/user.js
+++ src/models/user.js
@@ -3,11 +3,11 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username = { type: String, required: true },
-    password = { type: String, required: true },
-    createdAt = { type: Date },
-    updatedAt = { type: Date },
-    booksFromUser = [{ type: Schema.Types.ObjectId, ref: "Book" }]
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    booksFromUser: [{ type: Schema.Types.ObjectId, ref: "Book" }]
 });
 
 UserSchema.pre("save", function(next) {
