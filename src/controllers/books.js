9747388095bac0e02067a3237bd3fab514c8425a
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 12:40:27 -0400

    updated book controller

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -4,7 +4,23 @@ const Book = require('../models/books.js')
 
 // GET
 router.get("/", (req, res) => {
-    Book.find({}).then(book => {
-        res.json({ 'list of available books: ': book });
+    Book.find({}).then(books => {
+        res.json({ 'list of available books: ': books });
+    })
+    .catch(err => {
+        console.log(err.message);
+        res.json({ 'no books found, message: ': err.message });
     })
 })
+
+router.get("/:book", function(req, res) {
+    Book.find({ book: req.params.book })
+    .then(book => {
+        res.json({ 'title': book.title, 'author': book.author, 
+                    'year-published': book.pubYear,
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
