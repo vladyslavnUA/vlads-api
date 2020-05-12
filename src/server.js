586452adc67ad3864fca93fba5765d7258449682
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 15:25:00 -0400

    updated server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const util = require('util');
 const app = require('./config/express');
 const router = require('./controllers/index.js');
-
+require('./data/api-db');
 mongoose.Promise = Promise;
 
 // mongoose.connect(mongoUri)
@@ -16,7 +17,6 @@ if (!module.parent) {
     });
 }
 
-require('dotenv').config();
-require('./data/api-db');
+
 
 module.exports = app;
