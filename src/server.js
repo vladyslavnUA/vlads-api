69537b51a03b539d73068a0c0aa7f24a2628fcbe
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-12 12:45:04 -0400

    updated server file

--- src/server.js
+++ src/server.js
@@ -1,31 +1,22 @@
 const mongoose = require('mongoose');
 const util = require('util');
-
-require('dotenv').config();
-
 const app = require('./config/express');
 const router = require('./controllers/index.js');
 
 mongoose.Promise = Promise;
 
-const mongoUri = process.env.MONGODB_URI;
-// mongoose.connect(
-//   mongoUri,
-//   { server: { socketOptions: { keepAlive: 1 } } }
-// );
-// mongoose.connection.on('error', () => {
-//   throw new Error(`unable to connect to database: ${mongoUri}`);
-// });
+// mongoose.connect(mongoUri)
 
 app.use(router);
 
-mongoose.connect(mongoUri)
-
 if (!module.parent) {
     const port = process.env.PORT
-    app.listen(port, () => {
+    app.listen(process.env.PORT, () => {
         console.log(`app listening on port ${port}`);
     });
 }
+
+require('dotenv').config();
+require('./data/api-db');
 
 module.exports = app;
